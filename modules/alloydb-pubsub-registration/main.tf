1e48e8bf2cceb9d75f24e646cf21352f364263e1
Glenn <30859701+GlennLee-IBM@users.noreply.github.com>
2026-09-09 13:55:37 -0400

    GRD-123666 - Pin IBM/gdp/guardium module to = 1.3.3 and Terraform Module to = 1.5.2 for version control (#41)
    Signed-off-by: Glenn <glenn.lee@ibm.com>
    Co-authored-by: Glenn <bu6plz@gmail.com>

--- modules/alloydb-pubsub-registration/main.tf
+++ modules/alloydb-pubsub-registration/main.tf
@@ -22,6 +22,7 @@ locals {
 
 module "universal_connector" {
   source = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
+  version = "= 1.3.3"
   count  = var.enable_universal_connector ? 1 : 0 # Skip creation when disabled
 
   udc_name       = local.udc_name_safe
