2c9ee98a629f7a248ea22f65bbe8f3402fe90498
Glenn <glenn.lee@ibm.com>
2026-09-10 01:49:47 -0400

    1.3.3-beta -> 1.3.4

--- modules/azure-sql-jdbc-registration/main.tf
+++ modules/azure-sql-jdbc-registration/main.tf
@@ -27,7 +27,7 @@ locals {
 
 module "universal_connector" {
   source = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
-  version = "= 1.3.3-beta"
+  version = "= 1.3.4"
   count  = var.enable_universal_connector ? 1 : 0 # Skip creation when disabled
 
   udc_name       = local.udc_name_safe
