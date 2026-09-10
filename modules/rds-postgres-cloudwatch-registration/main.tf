9f751409b012c2ec2ae0639f886c9c2d068e3551
Glenn <glenn.lee@ibm.com>
2026-09-10 01:49:47 -0400

    1.3.3-beta -> 1.3.4
    Signed-off-by: Glenn <glenn.lee@ibm.com>

--- modules/rds-postgres-cloudwatch-registration/main.tf
+++ modules/rds-postgres-cloudwatch-registration/main.tf
@@ -23,11 +23,11 @@ locals {
 
 module "universal_connector" {
   source = "IBM/gdp/guardium//modules/connect-datasource-to-uc"
-  version = "= 1.3.3-beta"
+  version = "= 1.3.4"
   count  = var.enable_universal_connector ? 1 : 0  # Skip creation when disabled
   udc_name = local.udc_name_safe
   udc_csv_parsed = local.udc_csv
-  
+
   client_id              = var.gdp_client_id
   client_secret          = var.gdp_client_secret
   gdp_server             = var.gdp_server
